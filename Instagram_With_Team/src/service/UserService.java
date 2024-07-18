b4746a1d7514f3ffc74f3ac3ff8c006678809618
Zuhaprogrammer <azizprogrammer05@gmail.com>
2024-07-18 23:56:37 +0500

    done conflict

--- Instagram_With_Team/src/service/UserService.java
+++ Instagram_With_Team/src/service/UserService.java
@@ -5,14 +5,4 @@ import model.User;
 import java.util.ArrayList;
 
 public class UserService {
-    private static final String LAST_PATH = "users.json";
-    private final FileUtilService<User> fileUtilService;
-
-    public UserService (FileUtilService<User> fileUtilService) {
-        this.fileUtilService = fileUtilService;
-    }
-
-    public ArrayList<User> list() {
-        return fileUtilService.read(LAST_PATH);
-    }
 }
