ebed86b04c0014cfee5a5c995fbe13939320c270
Zuhaprogrammer <azizprogrammer05@gmail.com>
2024-07-18 23:49:47 +0500

    done postService and readService

--- Instagram_With_Team/src/service/UserService.java
+++ Instagram_With_Team/src/service/UserService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.User;
+
+import java.util.ArrayList;
+
 public class UserService {
+    private static final String LAST_PATH = "users.json";
+    private final FileUtilService<User> fileUtilService;
+
+    public UserService (FileUtilService<User> fileUtilService) {
+        this.fileUtilService = fileUtilService;
+    }
+
+    public ArrayList<User> list() {
+        return fileUtilService.read(LAST_PATH);
+    }
 }
